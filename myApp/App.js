cd5b3b89637381aca77980d4905b2c64b4ebd651
Bless Adeti <blessadeti7@gmail.com>
2024-05-26 15:41:54 +0000

    Edited text and increased font size

--- myApp/App.js
+++ myApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.mytext}>
+        My name is Bless Adeti
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mytext: {
+    fontSize: 25,
+  },
+  
 });
